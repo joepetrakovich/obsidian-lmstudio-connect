c7175c969571fc6bd119bf80835dcb306a6111ec
Joe Petrakovich <jpetrakovich@gmail.com>
2025-12-24 13:05:59 +0900

    refactor settings modal to use svelte, lmao

--- src/components/Icon.svelte.ts
+++ src/components/Icon.svelte.ts
@@ -3,6 +3,7 @@ import type { Attachment } from 'svelte/attachments';
 
 export function icon(name: string): Attachment {
 	return (element) => {
+		element.addClass('lmsc-icon');
 		setIcon(element as HTMLElement, name);
 	};
 }
